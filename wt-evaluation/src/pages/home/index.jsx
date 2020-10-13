0131e2d44a41a81a0e6fd03309586de7ab1b52d8
damian <damianezetiel1@gmail.com>
2020-10-13 04:24:57 -0300

    refactors and fixes in toolbar

--- wt-evaluation/src/pages/home/index.jsx
+++ wt-evaluation/src/pages/home/index.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import { Toolbar } from '../../components/toolbar';
+import { SocialBox } from '../../components/social-box';
+import { FaFacebook, FaInstagram } from 'react-icons/fa'
 import './home.scss';
 
 export const Home = () => {
+    const socialBoxData = {
+        title: 'SEGUÍ CONECTADO',
+        networks: [
+            {
+                icon: <FaFacebook />,
+                url: 'http://www.facebook.com/WundermanThompson'
+            },
+            {
+                icon: <FaInstagram />,
+                url: 'https://www.instagram.com/wunthompson/?hl=es-la'
+            }
+        ]
+    }
+
     return (
         <div className="home-page">
-            <Toolbar title="LandingPage" />
+            <Toolbar
+                title="LandingPage"
+                rightContent={
+                    <SocialBox
+                        title={socialBoxData.title}
+                        socialItems={socialBoxData.networks} />
+                }
+            />
         </div>
     )
 }
