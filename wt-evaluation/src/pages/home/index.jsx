5a96503dc8b463a5c6ce77a8515ea33d4e62579d
damian <damianezetiel1@gmail.com>
2020-10-13 02:07:01 -0300

    refactor structure

---
+++ wt-evaluation/src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './home.scss';
+
+export const Home = () => {
+    return (
+        <div className="home-page">
+            <p>home page</p>
+        </div>
+    )
+}
