f16be71ecc5b2ac80e2057062525055e49e56c3c
damian <damianezetiel1@gmail.com>
2020-10-14 19:34:00 -0300

    refactor in article box

--- wt-evaluation/src/components/articles-box/index.jsx
+++ wt-evaluation/src/components/articles-box/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useScreenSize } from '../../hooks/useScreenSize';
 import '../../theme/theme.scss';
 import './articles-box.scss';
 import './articles-box-mobile.scss';
 
 export const ArticlesBox = ({ items, title, className }) => {
 
+    // Use hook for get the screen size
+    const screenSize = useScreenSize();
+    const articleWidth = 240; // padding: 40px
+
+    // Article item 
     const Article = ({ img, title, info }) => {
         return (
             <div className="article">
@@ -17,12 +23,65 @@ export const ArticlesBox = ({ items, title, className }) => {
         )
     }
 
+    {
+        /* Create table content 
+        structure: trItems[tdItems[]]
+
+        Calculate the the available space for insert Article component.
+        */
+    }
+    const createTableContent = () => {
+        let structure = [[]];
+        let index = -1;
+
+        items.map((article) => {
+            if (index < Math.floor(screenSize.size.width / articleWidth - 1)) {
+                index++;
+                structure[structure.length - 1].push(
+                    <td>
+                        <Article
+                            img={article.img}
+                            title={article.title}
+                            info={article.info}
+                        />
+                    </td>
+                )
+            } else {
+                index = 0;
+                structure.push(
+                    [<td>
+                        <Article
+                            img={article.img}
+                            title={article.title}
+                            info={article.info}
+                        />
+                    </td>]
+                )
+            }
+        })
+
+        return structure.map(trItem => {
+            return (
+                <tr>
+                    {trItem.map((tdItem, i) => {
+                        return tdItem
+                    })}
+                </tr>
+            )
+        })
+    }
+
     return (
         <div className={`articles-box ${className}`}>
             {title && <p className="title theme-text">{title}</p>}
             <div className="articles">
 
-                {items.map(article => {
+                <table>
+                    {createTableContent()}
+                </table>
+
+                {/* easy way, but have problems */}
+                {/* {items.map(article => {
                     return (
                         <Article
                             img={article.img}
@@ -30,7 +89,7 @@ export const ArticlesBox = ({ items, title, className }) => {
                             info={article.info}
                         />
                     )
-                })}
+                })} */}
             </div>
         </div>
     );
